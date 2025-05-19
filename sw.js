66aa296e0247aa57fcb2bba47b2d8eee0e50e1fd
Michu224 <michalyoutube@onet.pl>
2025-05-19 21:44:18 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
